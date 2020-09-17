35baa257855c1c7b2573c473680e5a99c5525aca
PabloLopez1998 <35881455+PabloLopez1998@users.noreply.github.com>
2020-09-16 17:28:07 -0700

    Next: Find indoor video

--- restaurant/src/components/Experience.js
+++ restaurant/src/components/Experience.js
@@ -1,28 +1,56 @@
 import React from 'react';
 import './Experience.scss';
 import { motion } from 'framer-motion';
-//import imgOutDoor from '../Images/outdoor.jpg';
+import imgOne from '../Images/iconic.jpg';
+import imgTwo from '../Images/boattwo.jpg';
+import imgThree from '../Images/outside.jpg';
+import imgFour from '../Images/iconic.jpg';
+import imgFive from '../Images/iconic.jpg';
+import imgSix from '../Images/iconic.jpg';
+import ReactPlayer from 'react-player';
+import myVideo from '../Videos/video.mp4';
 
 function Experience() {
     return (
         <div className="experience-container">
             <div className="top">
-                <div className="first"><img /></div>
-                <div className="second"><img /></div>
-                <div className="third"><img /></div>
-                <div className="text"><img /></div>
+                <div className="first"><img src={imgOne} /></div>
+                <div className="second">
+                    <ReactPlayer
+                        className="react-player"
+                        url={myVideo}
+                        muted='true'
+                        volume='0'
+                        //playing='true'
+                        loop='true'
+                    />
+                </div>
+                <div className="third"><img src={imgTwo} /></div>
+                <span className="text">
+                    Florence is just right in so many ways.
+                    The city is just the right size to make it one of the most walkable cities in Italy.
+                    This city is the rightful birthplace of the Renaissance and is loaded with wonderful art, architecture, and some of the finest museums in the world, the Uffizi and the Galleria dell’ Accademia.
+                    Florence truly is of the most beautiful cities in the world.
+                    Tour the city and enjoy the scenery while rounding off the trip with a visit to Lorenzo's.
+                </span>
             </div>
             <div className="middle">
                 <div className="left"><img /></div>
-                <div className="right"><img /></div>
+                <div className="right"><img src={imgThree} /></div>
             </div>
             <div className="bottom">
-                <div className="first"><img /></div>
-                <div className="second"><img /></div>
-                <div className="third"><img /></div>
-                <div className="text"><img /></div>
+                <div className="first"><img src={imgFour} /></div>
+                <div className="second"><img src={imgFive} /></div>
+                <div className="third"><img src={imgSix} /></div>
+                <span className="text">
+                    Florence is just right in so many ways.
+                    The city is just the right size to make it one of the most walkable cities in Italy.
+                    This city is the rightful birthplace of the Renaissance and is loaded with wonderful art, architecture, and some of the finest museums in the world, the Uffizi and the Galleria dell’ Accademia.
+                    Florence truly is of the most beautiful cities in the world.
+                    Tour the city and enjoy the scenery while rounding off the trip with a visit to Lorenzo's.
+                </span>
             </div>
-            <div className="title"></div>
+            <span className="title">Experience<span style={{ color: "goldenrod" }}>.</span></span>
         </div>
     )
 }
